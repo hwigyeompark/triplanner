5e7964b200194905a8e40c81541f7aa5c62ae3b4
hwigyeompark <phg5590@gmail.com>
2018-05-18 13:39:40 +0900

    update Content

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,11 +1,23 @@
 import React, {Component} from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({text}) => <div>{text}</div>
 
 class Content extends Component{
+    static defaultProps = {
+        center:{
+            lat:59.95,
+            lng:30.33
+        },
+        zoom:11
+    };
     render(){
         return(
           <section>
               <div>
+                <GoogleMapReact
 
+                />
               </div>
           </section>
         );
