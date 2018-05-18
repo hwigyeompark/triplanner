e43a6ff992d7c31f2aeb5c89353335ab6774e712
hwigyeompark <phg5590@gmail.com>
2018-05-18 13:43:13 +0900

    update Content

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -16,8 +16,17 @@ class Content extends Component{
           <section>
               <div>
                 <GoogleMapReact
+                    URLKeys = {{ key : "AIzaSyDgWYXsjLiOk_LjeD36Q15aGXwPpuOJxTE" }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
 
-                />
+                      <AnyReactComponent
+                          lat={59.955413}
+                          lng={30.337844}
+                          text={'Kreyser Avroro'}
+                      />
+                </GoogleMapReact>
               </div>
           </section>
         );
