a28759df85a3b36382b76eec13d6c695f5e2fc6f
hwigyeompark <phg5590@gmail.com>
2018-05-18 14:08:35 +0900

    update Content

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,32 +1,25 @@
 import React, {Component} from 'react';
-import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = ({text}) => <div>{text}</div>
+import GoogleDistanceMatrix from 'google-distance-matrix';
 
 class Content extends Component{
     static defaultProps = {
-        center:{
-            lat:59.95,
-            lng:30.33
-        },
-        zoom:11
+        origins : 'San Francisco CA',
+        destinations : {
+            city : 'New York NY',
+            num : '41.8337329'
+        }
     };
+
     render(){
         return(
           <section>
-              <div>
-                <GoogleMapReact
+              <div style={{height:'100vh', width:'100%'}}>
+                <GoogleDistanceMatrix
                     URLKeys = {{ key : "AIzaSyDgWYXsjLiOk_LjeD36Q15aGXwPpuOJxTE" }}
-                    defaultCenter={this.props.center}
-                    defaultZoom={this.props.zoom}
+                    defaultOrigin={this.props.origins}
+                    defaultDestinations={this.props.destinations}
                 >
-
-                      <AnyReactComponent
-                          lat={59.955413}
-                          lng={30.337844}
-                          text={'Kreyser Avroro'}
-                      />
-                </GoogleMapReact>
+                </GoogleDistanceMatrix>
               </div>
           </section>
         );
