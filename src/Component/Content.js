a797b09179b4b9692daef84520e475bf9e6e0657
hwigyeompark <phg5590@gmail.com>
2018-05-21 11:47:26 +0900

    add Navbar

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,33 +1,10 @@
 import React, {Component} from 'react';
-import GoogleDistanceMatrix from 'google-distance-matrix';
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>
 
 class Content extends Component{
-    static defaultProps = {
-        origins : 'San Francisco CA',
-        destinations : {
-            city : 'New York NY',
-            num : '41.8337329'
-        }
-    };
-
     render(){
         return(
           <section>
-              <div style={{height:'100vh', width:'100%'}}>
-                <GoogleDistanceMatrix
-                    URLKeys = {{ key : "AIzaSyDgWYXsjLiOk_LjeD36Q15aGXwPpuOJxTE" }}
-                    defaultOrigin={this.props.origins}
-                    defaultDestinations={this.props.destinations}
-                >
-                    <AnyReactComponent
-                        city={'New York NY'}
-                        num={'41.8337329'}
-                        text={'fdafsf'}
-                    />
-                </GoogleDistanceMatrix>
-              </div>
+
           </section>
         );
     }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Navbar extends Component{
+    render(){
+        return(
+          <div>
+              <h1></h1>
+              <ul>
+                  <li>일정만들기</li>
+                  <li>여행기작성</li>
+              </ul>
+          </div>
+        );
+    }
+}
